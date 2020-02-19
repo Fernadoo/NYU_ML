aa2fe430d2ff0c308d6ceae80cc67fb066ab3c58
mac_zhu <zfm19980618@icloud.com>
2020-02-19 19:08:57 +0800

    refine grad checker

--- 1/hw1-sgd/hw1_skeleton_code.py
+++ 1/hw1-sgd/hw1_skeleton_code.py
@@ -119,8 +119,8 @@ def grad_checker(X, y, theta, epsilon=0.01, tolerance=1e-4):
     for i in range(num_features):
         delta = np.zeros(num_features)
         delta[i] = 1
-        j_plus = compute_square_loss(X, y, theta+np.dot(delta,epsilon))
-        j_minus = compute_square_loss(X, y, theta-np.dot(delta,epsilon))
+        j_plus = compute_square_loss(X, y, theta+delta*epsilon)
+        j_minus = compute_square_loss(X, y, theta-delta*epsilon)
         approx_grad[i] = (j_plus - j_minus) / (2 * epsilon)
     if np,norm(approx_grad - true_gradient, order=2) <= tolerance:
         return True
